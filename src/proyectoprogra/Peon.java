402fd901dcbe5fb8af88cf72d53d96f069a28f24
geohurtarte2015 <edgar.hurtarte@gmail.com>
2017-12-28 09:58:58 -0600

    Subida juego de ajedrez hexagonal comunitario

---
+++ src/proyectoprogra/Peon.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogra;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Dash
+ */
+public class Peon extends Pieza {
+
+    public Peon(int posx, int posy, int x, int y, int bando) {
+        super(bando, posx, posy, x, y);
+        if (bando == BLANCAS) {
+            this.setIcon(new ImageIcon("src/Images/peon2.png"));
+        } else {
+            this.setIcon(new ImageIcon("src/Images/peon.png"));
+        }
+    }
+
+    @Override
+    boolean validarMovimiento(int newx, int newy) {
+        if (bando == BLANCAS) {
+            if (x == newx) {
+                return true;
+            }
+        } else {
+            if (x == newx) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/proyectoprogra/Pieza.java
@@ -0,0 +1,31 @@
+package proyectoprogra;
+
+import java.awt.Point;
+
+abstract class Pieza extends Componente {
+    public static int BLANCAS = 0;
+    public static int NEGRAS = 1;
+    
+    protected int bando;
+    protected int x;
+    protected int y;
+
+    public Pieza(int bando, int posx, int posy, int x, int y) {
+        super(PIEZA, 50, posx, posy);
+        this.bando = bando;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void nuevaPosicion(Point p) {
+        this.setLocation(p.x + 7, p.y + 7);
+        
+    }
+
+    public void imprimirPosicion() {
+        System.out.println("Estoy seleccionando la pieza: " + x + ", " + y);
+    }
+
+    abstract boolean validarMovimiento(int newx, int newy);
+
+}
